cb5fd1bb38d7fa265e083e67c9882286b4064802
sameemul-haque <samhaqk@gmail.com>
2023-09-03 15:09:06 +0530

    added rofi for bluetooth and wifi

---
+++ .config/rofi/wifi/wifi.sh
@@ -0,0 +1,15 @@
+# #!/bin/bash
+# TO CONNECT TO WIFI USING DMENU | ROFI WHICH IS CONNECTED BEFORE
+# bssid=$(nmcli device wifi list | sed -n '1!P'| cut -b 9- | dmenu -p "Wifi" -l 10 | awk '{print $1}')
+
+dir="$HOME/.config/rofi"
+theme='style-1'
+
+# [ -z "$bssid" ] && exit 1
+# nmcli device wifi connect $bssid
+# notify-send "📶 WiFi Connected"
+
+bssid=$(nmcli device wifi list | sed -n '1!P'| cut -b 9- | rofi -dmenu -theme ${dir}/${theme}.rasi -p " " -lines 10 | awk '{print $1}')
+[ -z "$bssid" ] && exit 1
+nmcli device wifi connect $bssid
+notify-send "📶 WiFi Connected"
---
+++ .config/rofi/wifi/wifinew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# SCRIPT TO CONNECT TO NEW WIFI USING DMENU | ROFI
+
+dir="$HOME/.config/rofi"
+theme='style-1'
+
+# bssid=$(nmcli device wifi list | sed -n '1!P'| cut -b 9- | dmenu -p "Wifi" -l 10 | awk '{print $1}')
+# [ -z "$bssid" ] && exit 1
+# pass=$(echo "" | dmenu -p "Enter password")
+# [ -z "$pass" ] && notify-send "🔑 Password  not enterd" && exit 1
+# nmcli device wifi connect $bssid password $pass
+# notify-send "📶 New WiFi Connected"
+
+bssid=$(nmcli device wifi list | sed -n '1!P' | cut -b 9- | rofi -dmenu -theme ${dir}/${theme}.rasi -p " " -lines 10 | awk '{print $1}')
+[ -z "$bssid" ] && exit 1
+pass=$(echo "" | rofi -dmenu -theme-str 'textbox-prompt-colon {str: "";}' -theme ${dir}/${theme}.rasi -p "Enter password")
+[ -z "$pass" ] && notify-send "🔑 Password not entered" && exit 1
+nmcli device wifi connect $bssid password $pass
+notify-send "📶 New WiFi Connected"
