c4dfc89e20db72dadc4ccfd39a1114a8769625c2
sameemul-haque <samhaqk@gmail.com>
2023-09-16 22:52:04 +0530

    rofi

--- .config/rofi/powermenu/type-2/powermenu.sh
+++ .config/rofi/powermenu/type-2/powermenu.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Github : @adi1090x
-#
-## Rofi   : Power Menu
-#
-## Available Styles
-#
-## style-1   style-2   style-3   style-4   style-5
-## style-6   style-7   style-8   style-9   style-10
-
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-2"
 theme='style-5'
